3143bcd90f9cc10f3f22e599d7f7cabc2afd5c51
KOKMENKIT <56879028+KOKMENKIT@users.noreply.github.com>
2020-07-15 05:00:58 +0800

    New Add Activity

---
+++ app/src/main/java/com/example/laundryserviceapps/LaundryShop.kt
@@ -0,0 +1,11 @@
+package com.example.laundryserviceapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaundryShop : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laundry_shop)
+    }
+}
