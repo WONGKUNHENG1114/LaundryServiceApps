19aa98813077d6c552f4f8c1c610a8d6e4e594fd
KOKMENKIT <56879028+KOKMENKIT@users.noreply.github.com>
2020-07-15 05:20:25 +0800

    Updated

--- app/src/main/java/com/example/laundryserviceapps/Test.kt
+++ app/src/main/java/com/example/laundryserviceapps/Test.kt
@@ -2,16 +2,10 @@ package com.example.laundryserviceapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_laundry_shop.*
 
-class LaundryShop : AppCompatActivity() {
+class Test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_laundry_shop)
-
-        imgback7.setOnClickListener {
-
-        }
-
+        setContentView(R.layout.activity_test)
     }
 }
